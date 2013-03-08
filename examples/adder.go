d3cae5c58fdb574dba4ec7af213e8d97ed5b2d4f
Aaron Ackerman <theron17@gmail.com>
2013-03-07 20:59:58 -0600

    Added a lot of notes

--- examples/adder.go
+++ examples/adder.go
@@ -3,18 +3,15 @@ package main
 import "fmt"
 
 func adder(i int) func(int) int {
-  return func(k int) int {
-    k += i
-    fmt.Printf("%v\n", k);
-    return i
-  }
+	return func(k int) int {
+		k += i
+		fmt.Printf("%v\n", k)
+		return i
+	}
 }
 
 func main() {
-  addFive := adder(5)
-  addFive(2)
-  addFive(3)
-  addFive(4)
-  addFive(5)
-  addFive(6)
+	addFive := adder(5)
+	addFive(7)
+	addFive(4)
 }
